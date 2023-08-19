d538f653f76640b5f0f96e936f4f825cf9631159
AustinCGomez <austincgomez@gmail.com>
2023-08-19 02:28:52 -0500

    Version 0.1 Beta

---
+++ README.md
@@ -0,0 +1,43 @@
+# Simple Python File Mover (SPFM) (Under Active Development)
+## WARNING: Simple Python File Mover (SPFM) is currently in development beta and undergoing constant changes and fixes. It's not suggested or recommended to use this in a production environment yet.
+SPFM is a user-friendly Python program designed to streamline the process of moving files within your Windows PC.
+
+## Getting Started
+
+To utilize SPFM, ensure that you have Python installed on your Windows PC. If not, you can download the latest version from the official Python website ([Python Downloads](https://www.python.org/downloads/)).
+
+Follow these steps to get started:
+
+1. Clone or download the repository from our GitHub main branch. While you can also access the development branch, be mindful of potential untested bugs.
+2. Launch the command prompt in administrator mode.
+3. Enter the command `python main.py` to initiate the program. Then, follow the provided CLI instructions to execute your desired file-moving tasks.
+
+## Tools and Dependencies
+
+SPFM is built using Python and relies on the following packages, which should be pre-installed with your Python setup:
+
+- `os`
+- `tkinter`
+- `shutil`
+
+## Contributing
+
+Your contributions to SPFM are highly valued! If you identify any bugs or have suggestions for new features, please create an issue on GitHub or submit a pull request. Your efforts will be acknowledged in the list of contributors featured below.
+
+## Contributor Guidelines
+
+We eagerly welcome contributions to SPFM. Should you wish to contribute a bug fix or propose a feature, kindly follow these guidelines:
+
+1. Open an issue on GitHub to discuss your proposed changes.
+2. Submit a pull request with your modifications.
+3. Upon approval, your name will be listed among our esteemed contributors.
+
+### Our Incredible Contributors
+
+* **Austin Gomez** (GitHub: austinprog)
+
+## License
+
+SPFM operates under the MIT License. For the full license text, refer to the `LICENSE` file included within the repository.
+
+Experience the convenience of file management with SPFM—simple, efficient, and effective!
---
+++ main.py
@@ -0,0 +1,137 @@
+import os
+import shutil
+from tkinter import filedialog
+from tkinter import *
+
+class CommandAndControl: 
+    def __init__(self):
+        self.userInput = None
+        self.fromAddress = None
+        self.toAddress = None
+
+    #We will obtain our directory from here and then each class can call this to retrieve it when they need it at that time. 
+    @staticmethod
+    def obtainDirectory():
+        print("Opening a Graphical User Interface(GUI) to obtain your directory to move files FROM")
+        root = Tk()
+        root.withdraw()
+        from_directory = filedialog.askdirectory()
+        print(f"Success: We have obtained the directory: {from_directory}")
+        print("Opening a Graphical User Interface(GUI) to obtain your directory to move files FROM")
+        root = Tk()
+        root.withdraw()
+        to_directory = filedialog.askdirectory()
+        print(f"Success: We have obtained the directory: {to_directory}")
+        return from_directory, to_directory
+
+    def begin(self):
+        print("Simple Python File Mover 0.1 Alpha")
+        print("OPTIONS: -s Search directory and return all files in the specific directory || -m Move files from one directory to another directory")
+        self.userInput = input("Please enter your desired command here: ").lower()
+        #A simple input validation approach
+        if self.userInput == "-s":
+            #Create new FileSearcher Object
+            newFileSearch = FileSearcher()
+            newFileSearch.search_directory()
+
+        elif self.userInput == '-m':
+            #Create new MoveFiles Object 
+            newMove = MoveFiles()
+            newMove.obtain_directory()
+        else: 
+            print("Sorry, you did not enter valid information")
+
+
+class FileSearcher:
+    def __init__(self):
+        self.directory_from_retrievel = None
+        self.directory_to_retrievel = None
+        self.file_extensions = {}
+
+    def search_directory(self):
+        self.directory_from_retrievel, self.directory_to_retrievel = CommandAndControl.obtainDirectory() 
+        print(self.directory_from_retrievel)
+        print(self.directory_to_retrievel)
+        print("Testing the static method")
+        while True:
+            if os.path.isdir(self.directory_from_retrievel):
+                print("Listing all files grouped by their extensions in the specified directory:")
+                print(self.directory_from_retrievel)
+                print("Excellent news")
+                self._group_files_by_extension()
+                self._print_files_by_extension()
+                self._print_entire_directories()
+                break
+            else:
+                print("Invalid directory path. Please enter a valid one.")
+
+    def _group_files_by_extension(self):
+        for filename in os.listdir(self.directory_from_retrievel):
+            if os.path.isfile(os.path.join(self.directory_from_retrievel, filename)):
+                _, ext = os.path.splitext(filename)
+                self.file_extensions.setdefault(ext, []).append(filename)
+
+    def _print_files_by_extension(self):
+        for ext, files in self.file_extensions.items():
+            print(f"Extension: {ext}")
+            print('\n'.join(files))
+            print() 
+
+    def _print_entire_directories(self):
+        for entry in os.scandir(self.directory_from_retrievel):
+            if entry.is_dir():
+                print(f"Folders: {entry}")
+
+
+class MoveFiles:
+    def __init__(self):
+        self.folder_from = None
+        self.folder_to = None
+
+# This method will obtain the directory with the code already defined in the static method. 
+    def obtain_directory(self):
+        self.folder_from, self.folder_to = CommandAndControl.obtainDirectory()
+        MoveFiles.move_the_files(self)
+
+
+   # def display_file_extensions_in_directory(self):
+    #    file_searcher = FileSearcher()
+     #   file_searcher.directory = self.folder_selected
+      #  file_searcher.search_directory()
+
+    def move_the_files(self):
+        self.extension_list = []
+        while True:
+            self.getInput = input("Please enter each file extension that you want to move from the directory. Please type 'quit' when you are done")
+        
+            if self.getInput == 'quit':
+                break
+                
+            self.extension_list.append(self.getInput)
+
+        # Display the list of all of the file extensions that the user entered. 
+        print("Here are all of the file extensions that you chose to move:")
+        for extension in self.extension_list:
+            print(extension)
+
+        print("We will now attempt to move the files based on extension from the list given..")
+        # This will need to be modified eventually so that it can interact with the other classes to already have the directories? 
+        # this is just our path that we are going to use for testing purposes. 
+        print("folder_from")
+        print(self.folder_from)
+        for file in os.listdir(self.folder_from):
+            if any(file.endswith(ext) for ext in self.extension_list):
+                src_path = os.path.join(self.folder_from, file)
+                dest_path = os.path.join(self.folder_to, file)
+                shutil.move(src_path, dest_path)
+                print(f"Moved {file} to {dest_path}")
+                print(f"Removed {file} in {src_path}")
+            
+                
+if __name__ == "__main__":
+    BeginProgram = CommandAndControl()
+    BeginProgram.begin()
+    #BeginProgram.obtainDirectory()
+    #newMover = MoveFiles()
+    #newMover.obtain_directory()
+    #newMover.move_the_files()
